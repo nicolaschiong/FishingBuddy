f0d60b41a2a52d1badfeb26edd908dd0cff51670
Nicolas James Chiong <nicolasjameschiong@Nicolass-MacBook-Pro.local>
2020-11-30 21:44:32 +0800

    Test Commit

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ const navigator = createStackNavigator(
   {
     initialRouteName: "Home",
     defaultNavigationOptions: {
-      title: "FishingBuddy"
+      title: "FishingBuddyShippuden"
     }
   }
 );
