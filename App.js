84ef9c732be9b60f948eab6e01fc4cc3d841d4fc
Nicolas James Chiong <nicolasjameschiong@Nicolass-MacBook-Pro.local>
2020-12-01 20:29:40 +0800

    Add React UI Kit

--- App.js
+++ App.js
@@ -1,17 +1,69 @@
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
-import HomeScreen from "./src/screens/HomeScreen";
+import React from "react";
+import { StyleSheet } from "react-native";
 
-const navigator = createStackNavigator(
-  {
-    Home: HomeScreen
-  },
-  {
-    initialRouteName: "Home",
-    defaultNavigationOptions: {
-      title: "FishingBuddyShippuden"
+import { AppLoading } from "expo";
+import { Asset } from "expo-asset";
+
+import Navigation from "./navigation";
+import { Block } from "./components";
+
+// import all used images
+const images = [
+  require("./assets/icons/back.png"),
+  require("./assets/icons/plants.png"),
+  require("./assets/icons/seeds.png"),
+  require("./assets/icons/flowers.png"),
+  require("./assets/icons/sprayers.png"),
+  require("./assets/icons/pots.png"),
+  require("./assets/icons/fertilizers.png"),
+  require("./assets/images/plants_1.png"),
+  require("./assets/images/plants_2.png"),
+  require("./assets/images/plants_3.png"),
+  require("./assets/images/explore_1.png"),
+  require("./assets/images/explore_2.png"),
+  require("./assets/images/explore_3.png"),
+  require("./assets/images/explore_4.png"),
+  require("./assets/images/explore_5.png"),
+  require("./assets/images/explore_6.png"),
+  require("./assets/images/illustration_1.png"),
+  require("./assets/images/illustration_2.png"),
+  require("./assets/images/illustration_3.png"),
+  require("./assets/images/avatar.png")
+];
+
+export default class App extends React.Component {
+  state = {
+    isLoadingComplete: false
+  };
+
+  handleResourcesAsync = async () => {
+    // we're caching all the images
+    // for better performance on the app
+
+    const cacheImages = images.map(image => {
+      return Asset.fromModule(image).downloadAsync();
+    });
+
+    return Promise.all(cacheImages);
+  };
+
+  render() {
+    if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
+      return (
+        <AppLoading
+          startAsync={this.handleResourcesAsync}
+          onError={error => console.warn(error)}
+          onFinish={() => this.setState({ isLoadingComplete: true })}
+        />
+      );
     }
-  }
-);
 
-export default createAppContainer(navigator);
+    return (
+      <Block white>
+        <Navigation />
+      </Block>
+    );
+  }
+}
+
+const styles = StyleSheet.create({});
